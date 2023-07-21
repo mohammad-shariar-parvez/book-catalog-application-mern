f0c6f2e1294e931bbd11013612a21aecff033b87
Piaowei <msptonmoy@gmail.com>
2023-07-21 14:40:50 +0600

    Ready for production

--- src/components/books/EditBook.tsx
+++ src/components/books/EditBook.tsx
@@ -164,7 +164,7 @@ const EditBook = () => {
           />
         </div>
 
-        <div className="gap-x-6 space-x-6">
+        <div className="gap-x-6 space-x-6 flex">
           <button
             type="submit"
             className="text-white bg-golden hover:bg-yellow-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800  "
